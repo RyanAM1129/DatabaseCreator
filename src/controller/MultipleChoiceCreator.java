40eb4eb3e00f1e1a77cd4429690880040ae698e7
RyanAM1129 <RyanAMacLeod@hotmail.com>
2022-05-07 19:03:28 -0700

    commit two

---
+++ src/controller/MultipleChoiceCreator.java
@@ -0,0 +1,39 @@
+package controller;
+
+import org.sqlite.SQLiteDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MultipleChoiceCreator {
+    SQLiteDataSource myDataSource;
+    Connection myConnection;
+    Statement myStatement;
+
+    public void establishConnection(final String theURL) {
+        try {
+            myDataSource = new SQLiteDataSource();
+            myDataSource.setUrl(theURL);
+            myConnection = myDataSource.getConnection();
+            myStatement = myConnection.createStatement();
+        } catch (Exception theException) {
+            theException.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    public void createTable(final String theName) {
+        final String myQuery = "CREATE TABLE IF NOT EXISTS " + theName + " ( " +
+                "QUESTION TEXT NOT NULL, CORRECT TEXT NOT NULL," +
+                "WRONG1 TEXT NOT NULL, WRONG2 TEXT NOT NULL," +
+                "WRONG3 TEXT NOT NULL )";
+
+        try {
+            myStatement.executeUpdate(myQuery);
+        } catch (SQLException theException) {
+            theException.printStackTrace();
+            System.exit(0);
+        }
+    }
+}
